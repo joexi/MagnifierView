730bcb7dcf94339e4366a21806ca75bb95181b3f
joexi <407154812@qq.com>
2014-04-25 15:52:50 +0800

    Create MagnifierView.h

---
+++ MagnifierView.h
@@ -0,0 +1,17 @@
+//
+//  MagnifierView.h
+//  SimplerMaskTest
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MagnifierView : UIView {
+	UIView *viewToMagnify;
+	CGPoint touchPoint;
+}
+
+@property (nonatomic, retain) UIView *viewToMagnify;
+@property (nonatomic, assign) CGPoint touchPoint;
+
++ (MagnifierView *)shared;
+@end
